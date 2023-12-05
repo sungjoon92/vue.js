d810aeec3ad6bf7a93b1f3e5c10acd157d8b9732
sungjoon92 <wsjtjdwns92@naver.com>
2023-12-05 19:42:47 +0900

    vue 프로젝트

--- project05_user3/src/views/user3/CreateView.vue
+++ project05_user3/src/views/user3/CreateView.vue
@@ -1,71 +1,109 @@
 <template>
   <div>
-    <!-- ListView.vue -->
     <h1>사용자 추가 (CreateView)</h1>
     <p>
       <router-link to="/user3/list" class="btn btn-primary">사용자 목록</router-link>
     </p>
     <div class="container">
-        <form action="">
-            <table class="table table-condensed">
-              <tr>
-                  <th>
-                      <label for="name">이름:</label>
-                  </th>
-                  <td>
-                      <input type="text" id="name" name="name" placeholder="이름 입력하세요" class="form-control" autocomplete="off">
-                  </td>
-              </tr>
-                <tr>
-                  <th>
-                      <label for="email">이메일:</label>
-                  </th>
-                  <td>
-                      <input type="email" id="email" name="email" placeholder="이메일 입력하세요" class="form-control" autocomplete="off">
-                  </td>
-              </tr>
-              <tr>
-                  <th>
-                      <label for="pwd">비밀번호:</label>
-                  </th>
-                  <td>
-                      <input type="password" id="pwd" name="pwd" placeholder="비밀번호 입력하세요" class="form-control" autocomplete="off">
-                  </td>
-              </tr>
-              <tr>
-                  <th>
-                      성별:
-                  </th>
-                  <td>
-                      <input type="radio" id="radio1" name="gender" value="1" checked>
-                      <label for="radio1">남성</label>
-                      &nbsp;&nbsp;&nbsp;
-                      <input type="radio" id="radio2" name="gender" value="0" checked>
-                      <label for="radio2">여성</label>
-                  </td>
-              </tr>
-            </table>
-        </form>
+      <form>
+        <table class="table table-condensed">
+          <tr>
+            <th>
+              <label for="name">이름:</label>
+            </th>
+            <td>
+              <input type="text" id="name" name="name" placeholder="이름 입력하세요" class="form-control" autocomplete="off"
+                v-model="result.name" :class="{ active: active.name }">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="email">이메일:</label>
+            </th>
+            <td>
+              <input type="email" id="email" name="email" placeholder="이메일 입력하세요" class="form-control" autocomplete="off"
+                v-model="result.email" :class="{ active: active.email }">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <label for="pwd">비밀번호:</label>
+            </th>
+            <td>
+              <input type="password" id="pwd" name="pwd" placeholder="비밀번호 입력하세요" class="form-control" autocomplete="off"
+                v-model="result.pwd" :class="{ active: active.pwd }">
+            </td>
+          </tr>
+          <tr>
+            <th>
+              성별:
+            </th>
+            <td>
+              <input type="radio" id="radio1" name="gender" value="1" checked v-model="result.gender">
+              <label for="radio1">남성</label>
+              &nbsp;&nbsp;&nbsp;
+              <input type="radio" id="radio2" name="gender" value="0" checked v-model="result.gender">
+              <label for="radio2">여성</label>
+            </td>
+          </tr>
+        </table>
+      </form>
+      <div>
+        <button type="button" class="btn btn-warning" @click="save">생성</button>
       </div>
-    <div>
-        <button class="btn btn-warning">생성</button>
     </div>
   </div>
 </template>
-
 <script>
-// 주의사항 AXIOS모듈 임포트후 사용하지 않으면 에러발생
+import axios from 'axios'
 // import axios from 'axios'
 export default {
+  name: 'CreateView',
   data () {
     return {
+      result: { name: '', email: '', pwd: '', gender: '' },
+      active: { name: false, email: false, pwd: false }
     }
   },
+  created () {
+  },
   methods: {
+    save () {
+      if (this.result.name === '') {
+        alert('이름 입력해 주세요~')
+        this.active.name = true
+        return
+      } else {
+        this.active.name = false
+      }
+
+      if (this.result.email === '') {
+        alert('이메일 입력해 주세요~')
+        this.active.email = true
+        return
+      } else {
+        this.active.email = false
+      }
+      if (this.result.pwd === '') {
+        alert('비밀번호 이름 입력해 주세요~')
+        this.active.pwd = true
+        return
+      } else {
+        this.active.pwd = false
+      }
+      console.log(this.result)
+      axios
+        .put('http://localhost:9095/user3/save', this.result)
+        .then((res) => {
+          console.log(res)
+          alert('사용자가 추가되었습니다')
+          this.$router.push({ name: 'ListView' })
+        })
+        .catch((err) => console.log(err))
+    }
   }
 }
 </script>
-
 <style>
- @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
+@import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
 </style>
--- project05_user3/src/views/user3/ListView.vue
+++ project05_user3/src/views/user3/ListView.vue
@@ -1,37 +1,70 @@
 <template>
   <div>
-    <!-- ListView.vue -->
     <h1>사용자 목록 (ListView)</h1>
     <p>
       <button class="btn btn-danger" @click="getData">AXIOS모듈 테스트</button>
-      <router-link to="/user3/create" class="btn btn-primary">사용자 추가</router-link>
+      <router-link to="/user3/create" class="btn btn-success">사용자 추가</router-link>
     </p>
+    <div class="container">
+      <table class="table table-hover">
+        <thead>
+          <tr class="info">
+            <th>이름</th>
+            <th>이메일</th>
+            <th>가입날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in result" v-bind:key="row.no" v-on:click="$event => href(row)">
+            <td>{{ row.name }}</td>
+            <td>{{ row.email }}</td>
+            <td>{{ row.regdate }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
-
 <script>
-// 주의사항 AXIOS모듈 임포트후 사용하지 않으면 에러발생
 import axios from 'axios'
+import store from '@/store'
 export default {
+  name: 'ListView',
   data () {
     return {
+      result: []
     }
+  },
+  created () {
+    this.getData()
   },
   methods: {
     getData () {
       axios
-        .get('http://localhost:9095/test')
+        .post('http://localhost:9095/user3/list')
         .then((response) => {
-          console.log(response)
+          this.result = response.data.result
         })
         .catch((error) => {
           console.log(error)
         })
+    },
+    href (row) {
+      // console.log(row)
+      // query -> http://localhost:8080/user/findById?name=아이티윌&pwd=1234
+      // this.$router.push({ name: 'SelectView', query: row })
+      // params -> http://localhost:8080/user/findById/아이티윌/1234
+      // index.js 파일에서 path: '/user/findById/:name/:pwd' 정의해야 함
+      // this.$router.push({ name: 'SelectView', params: row })
+
+      // store기능를 사용하기 위해 vuex설치해야 함. package.json에서 vuex모듈 설치되었는지 확인
+      // >npm install vuex
+      store.commit('setUser', row)
+      this.$router.push({ name: 'SelectView' })
     }
   }
 }
 </script>
-
 <style>
- @import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
+@import 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css';
 </style>
